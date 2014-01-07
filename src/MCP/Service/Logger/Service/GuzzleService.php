274c1ce448c87822b9f51a2ba3b969153964b052
Steve Kluck <stevekluck@quickenloans.com>
2014-01-07 08:46:19 -0500

    Add Guzzle http service

---
+++ src/MCP/Service/Logger/Service/GuzzleService.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * @copyright ©2005—2013 Quicken Loans Inc. All rights reserved. Trade Secret,
+ *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
+ *    is strictly prohibited.
+ */
+
+namespace MCP\Service\Logger\Service;
+
+use Guzzle\Common\Exception\GuzzleException;
+use Guzzle\Http\ClientInterface;
+use Guzzle\Http\Message\RequestInterface;
+use Guzzle\Http\Message\Response;
+use MCP\Service\Logger\Exception;
+use MCP\Service\Logger\MessageInterface;
+use MCP\Service\Logger\RendererInterface;
+use MCP\Service\Logger\ServiceInterface;
+
+/**
+ * @internal
+ */
+class GuzzleService implements ServiceInterface
+{
+    /**#@+
+     * @var string
+     */
+    const ERR_MISSING_URL = 'The Http Client is missing a base url.';
+    const ERR_RESPONSE_CODE = "The service responded with an unexpected http code: '%s'.";
+    /**#@-*/
+
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
+
+    /**
+     * @var RendererInterface
+     */
+    private $renderer;
+
+    /**
+     * @var boolean
+     */
+    private $isSilent;
+
+    /**
+     * @param ClientInterface $client
+     * @param RendererInterface $renderer
+     * @param boolean $isSilent
+     */
+    public function __construct(ClientInterface $client, RendererInterface $renderer, $isSilent = false)
+    {
+        $this->client = $client;
+        $this->renderer = $renderer;
+        $this->isSilent = $isSilent;
+
+        if (!$this->client->getBaseUrl()) {
+            throw new Exception(self::ERR_MISSING_URL);
+        }
+    }
+
+    /**
+     * @param MessageInterface $message
+     * @return null
+     */
+    public function send(MessageInterface $message)
+    {
+        $request = $this->client->post(
+            null,
+            null,
+            call_user_func($this->renderer, $message),
+            ['Content-Type' => 'text/xml']
+        );
+
+        if ($this->isSilent) {
+            $this->fireAndForget($request);
+
+        } else {
+            $response = $request->send();
+            if ($response->getResponseCode() !== 200) {
+                throw new Exception(sprintf(self::ERR_RESPONSE_CODE, $response->getResponseCode()));
+            }
+        }
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @return null
+     */
+    private function fireAndForget(RequestInterface $request)
+    {
+        try {
+            $request->send();
+
+        } catch (GuzzleException $e) {
+            error_log($e->getMessage());
+        }
+    }
+}
---
+++ tests/MCP/Service/Logger/Service/GuzzleServiceTest.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * @copyright ©2005—2013 Quicken Loans Inc. All rights reserved. Trade Secret,
+ *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
+ *    is strictly prohibited.
+ */
+
+namespace MCP\Service\Logger\Service;
+
+use Guzzle\Http\Exception\RequestException;
+use Mockery;
+use PHPUnit_Framework_TestCase;
+
+class GuzzleServiceTest extends PHPUnit_Framework_TestCase
+{
+    public static $logSetting;
+
+    public static function setUpBeforeClass()
+    {
+        self::$logSetting = ini_get('error_log');
+        ini_set('error_log', 'syslog');
+    }
+
+    public static function tearDownAfterClass()
+    {
+        ini_set('error_log', self::$logSetting);
+    }
+
+    /**
+     * @expectedException MCP\Service\Logger\Exception
+     * @expectedExceptionMessage The Http Client is missing a base url
+     */
+    public function testMissingUrlThrowsException()
+    {
+        $client = Mockery::mock('Guzzle\Http\ClientInterface', array('getBaseUrl' => ''));
+        $renderer = Mockery::mock('MCP\Service\Logger\RendererInterface');
+
+        $service = new GuzzleService($client, $renderer);
+    }
+
+    /**
+     * @expectedException MCP\Service\Logger\Exception
+     * @expectedExceptionMessage The service responded with an unexpected http code: '404'
+     */
+    public function testServiceReceivesNon200ResponseThrowsException()
+    {
+        $renderer = Mockery::mock('MCP\Service\Logger\RendererInterface');
+        $message = Mockery::mock('MCP\Service\Logger\MessageInterface');
+
+        $response = Mockery::mock('Guzzle\Http\Message\Response', ['getResponseCode' => 404]);
+        $request = Mockery::mock('Guzzle\Http\Message\RequestInterface', ['send' => $response]);
+
+        $client = Mockery::mock('Guzzle\Http\ClientInterface', array('getBaseUrl' => '/some/url'));
+        $client
+            ->shouldReceive('post')
+            ->with(null, null, 'rendered message', ['Content-Type' => 'text/xml'])
+            ->andReturn($request)
+            ->once();
+
+        $renderer
+            ->shouldReceive('__invoke')
+            ->once()
+            ->with($message)
+            ->andReturn('rendered message');
+
+        $service = new GuzzleService($client, $renderer);
+        $service->send($message);
+    }
+
+    /**
+     * @expectedException Guzzle\Http\Exception\RequestException
+     * @expectedExceptionMessage msg
+     */
+    public function testExceptionIsNotCaughtWhenServiceThrowsHttpException()
+    {
+        $renderer = Mockery::mock('MCP\Service\Logger\RendererInterface');
+        $message = Mockery::mock('MCP\Service\Logger\MessageInterface');
+
+        $request = Mockery::mock('Guzzle\Http\Message\RequestInterface');
+        $request
+            ->shouldReceive('send')
+            ->andThrow(new RequestException('msg'));
+
+        $client = Mockery::mock('Guzzle\Http\ClientInterface', array('getBaseUrl' => '/some/url'));
+        $client
+            ->shouldReceive('post')
+            ->with(null, null, 'rendered message', ['Content-Type' => 'text/xml'])
+            ->andReturn($request)
+            ->once();
+
+        $renderer
+            ->shouldReceive('__invoke')
+            ->once()
+            ->with($message)
+            ->andReturn('rendered message');
+
+        $service = new GuzzleService($client, $renderer);
+        $service->send($message);
+    }
+
+    public function testServiceReceivesNon200ResponseReturnsNullWhenSilent()
+    {
+        $renderer = Mockery::mock('MCP\Service\Logger\RendererInterface');
+        $message = Mockery::mock('MCP\Service\Logger\MessageInterface');
+
+        $response = Mockery::mock('Guzzle\Http\Message\Response', ['getResponseCode' => 500]);
+        $request = Mockery::mock('Guzzle\Http\Message\RequestInterface', ['send' => $response]);
+
+        $client = Mockery::mock('Guzzle\Http\ClientInterface', array('getBaseUrl' => '/some/url'));
+        $client
+            ->shouldReceive('post')
+            ->andReturn($request)
+            ->once();
+
+        $renderer
+            ->shouldReceive('__invoke')
+            ->once();
+
+        $service = new GuzzleService($client, $renderer, true);
+        $this->assertNull($service->send($message));
+    }
+
+    public function testServiceThrowsHttpExceptionResponseReturnsNullWhenSilent()
+    {
+        $renderer = Mockery::mock('MCP\Service\Logger\RendererInterface');
+        $message = Mockery::mock('MCP\Service\Logger\MessageInterface');
+
+        $request = Mockery::mock('Guzzle\Http\Message\RequestInterface');
+        $request
+            ->shouldReceive('send')
+            ->andThrow(new RequestException('msg'));
+
+        $client = Mockery::mock('Guzzle\Http\ClientInterface', array('getBaseUrl' => '/some/url'));
+        $client
+            ->shouldReceive('post')
+            ->andReturn($request)
+            ->once();
+
+        $renderer
+            ->shouldReceive('__invoke')
+            ->once();
+
+        $service = new GuzzleService($client, $renderer, true);
+        $this->assertNull($service->send($message));
+    }
+}
