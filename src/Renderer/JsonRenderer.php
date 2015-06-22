bd750ef50a9014ef8cc1caafc9bea3d61965175f
Steve Kluck <stevekluck@quickenloans.com>
2015-06-22 09:24:46 -0400

    Update json renderer to conform to spec defined for splunk

--- src/Renderer/JsonRenderer.php
+++ src/Renderer/JsonRenderer.php
@@ -12,6 +12,10 @@ use MCP\Logger\MessageInterface;
 use MCP\Logger\RendererInterface;
 
 /**
+ * Data is formatted differently for JSON, which is meant to be sent to splunk.
+ *
+ * @see https://confluence/display/CORE/Logging+to+Splunk
+ *
  * @internal
  */
 class JsonRenderer implements RendererInterface
@@ -26,38 +30,37 @@ class JsonRenderer implements RendererInterface
         $data = [];
 
         // Required
-        $this->addProperty($data, 'ApplicationId', $this->sanitizeInteger($message->applicationId()));
+        $this->addProperty($data, 'AppID', $this->sanitizeInteger($message->applicationId()));
 
         $date = ($message->createTime() !== null) ? $message->createTime()->format(DateTime::RFC3339, 'UTC') : null;
-        $this->addProperty($data, 'CreateTime', $date);
+        $this->addProperty($data, 'Created', $date);
 
-        $this->addProperty($data, 'Level', $this->sanitizeString($message->level()));
+        $this->addProperty($data, 'Level', $this->sanitizeString(strtolower($message->level())));
         $this->addProperty($data, 'Message', $this->sanitizeString($message->message()));
 
-        $this->addProperty($data, 'IsUserDisrupted', $this->sanitizeBoolean($message->isUserDisrupted()));
+        $this->addProperty($data, 'UserIsDisrupted', $this->sanitizeBoolean($message->isUserDisrupted()));
         $this->addExtendedProperties($data, $message->extendedProperties());
 
         $ip = ($message->machineIPAddress() !== null) ? $message->machineIPAddress()->asString() : null;
-        $this->addProperty($data, 'MachineIPAddress', $ip);
-        $this->addProperty($data, 'MachineName', $this->sanitizeString($message->machineName()));
+        $this->addProperty($data, 'ServerIP', $ip);
+        $this->addProperty($data, 'ServerHostname', $this->sanitizeString($message->machineName()));
 
         // Optional
-        $this->addOptionalProperty($data, 'ExceptionData', $this->sanitizeString($message->exceptionData()));
+        $this->addOptionalProperty($data, 'Exception', $this->sanitizeString($message->exceptionData()));
 
         $this->addOptionalProperty($data, 'AffectedSystem', $this->sanitizeString($message->affectedSystem()));
-        $this->addOptionalProperty($data, 'CategoryId', $this->sanitizeInteger($message->categoryId()));
+        $this->addOptionalProperty($data, 'Category', $this->sanitizeInteger($message->categoryId()));
         $this->addOptionalProperty($data, 'Referrer', $this->sanitizeString($message->referrer()));
-        $this->addOptionalProperty($data, 'RequestMethod', $this->sanitizeString($message->requestMethod()));
-        $this->addOptionalProperty($data, 'Url', $this->sanitizeString($message->url()));
-        $this->addOptionalProperty($data, 'UserAgentBrowser', $this->sanitizeString($message->userAgentBrowser()));
-        $this->addOptionalProperty($data, 'UserCommonId', $this->sanitizeInteger($message->userCommonId()));
+        $this->addOptionalProperty($data, 'Method', $this->sanitizeString($message->requestMethod()));
+        $this->addOptionalProperty($data, 'URL', $this->sanitizeString($message->url()));
+        $this->addOptionalProperty($data, 'UserAgent', $this->sanitizeString($message->userAgentBrowser()));
+        $this->addOptionalProperty($data, 'UserID', $this->sanitizeInteger($message->userCommonId()));
         $this->addOptionalProperty($data, 'UserDisplayName', $this->sanitizeString($message->userDisplayName()));
 
         $ip = ($message->userIPAddress() !== null) ? $message->userIPAddress()->asString() : null;
-        $this->addOptionalProperty($data, 'UserIPAddress', $ip);
+        $this->addOptionalProperty($data, 'UserIP', $ip);
 
         $this->addOptionalProperty($data, 'UserName', $this->sanitizeString($message->userName()));
-        $this->addOptionalProperty($data, 'UserScreenName', $this->sanitizeString($message->userScreenName()));
 
         return json_encode($data);
     }
@@ -87,7 +90,7 @@ class JsonRenderer implements RendererInterface
             $extended[$k] = $this->sanitizeString($prop);
         }
 
-        $this->addProperty($data, 'ExtendedProperties', $extended);
+        $this->addProperty($data, 'Properties', $extended);
     }
 
     /**
