4503b51bcf45cd3f55458b4444ad296ce2fefefc
Matt Colf <matthewcolf@quickenloans.com>
2015-06-30 17:44:40 -0400

    Improve Kinesis error handling.

--- src/Service/KinesisService.php
+++ src/Service/KinesisService.php
@@ -2,11 +2,12 @@
 
 namespace MCP\Logger\Service;
 
-use Aws\Kinesis\KinesisClient;
+use MCP\Logger\Exception;
 use MCP\Logger\RendererInterface;
 use MCP\Logger\ServiceInterface;
 use MCP\Logger\MessageInterface;
-use MCP\Logger\Exception;
+use Aws\Kinesis\KinesisClient;
+use Aws\Kinesis\Exception\KinesisException;
 
 /**
  * Logging service for Amazon Kinesis
@@ -17,9 +18,15 @@ class KinesisService implements ServiceInterface
 {
     use BufferedServiceTrait;
 
-    const ERR_BUFFER = 'Buffer size must be between 0 and 499.';
+    const ERR_BUFFER = 'Buffer size must be between 1 and 499.';
+    const ERR_ATTEMPTS = 'Number of attempts must be 1 or greater.';
+    const ERR_SIZE = 'Log message exceeds 1MB in size. Cannot be sent to Kinesis. Discarding. %s';
+    const ERR_UNKNOWN = 'Unknown error when sending messages to Kinesis.';
     const ERR_RESPONSE = 'Received improperly formatted response when sending messages.';
-    const ERR_BATCH = 'Unable to send %s (of %s) log messages after %s attempts. Check that the Kinesis stream exists and has enough shards.';
+    const ERR_BATCH = 'Unable to send %s (of %s) log messages after %s attempts. Service returned errors. %s';
+    const ERR_MULTIPLE = "Encountered %s errors when sending log messages.\n%s";
+
+    const SIZE_MAX = 950000;
 
     const STREAM_DEFAULT = 'Logger';
     const ATTEMPTS_DEFAULT = 5;
@@ -74,6 +81,10 @@ class KinesisService implements ServiceInterface
         $this->silent = $silent;
         $this->attempts = $attempts;
 
+        if ($attempts < 1) {
+            throw new Exception(self::ERR_ATTEMPTS);
+        }
+
         if ($bufferLimit < 0 || $bufferLimit > 499) {
             throw new Exception(self::ERR_BUFFER);
         }
@@ -112,50 +123,90 @@ class KinesisService implements ServiceInterface
     private function handleBatch(array $messages)
     {
         $total = count($messages);
+        $errors = [];
+
+        // discard messages over 1MB in size
+        $messages = array_filter($messages, function (array $message) use (&$errors) {
+            if (strlen($message['Data']) > self::SIZE_MAX) {
+                $errors[] = sprintf(self::ERR_SIZE, substr(base64_decode($message['Data']), 0, 500) . '...');
+                return false;
+            }
+
+            return true;
+        });
+
         $attempts = 0;
 
-        do {
+        while (count($messages) > 0 && $attempts < $this->attempts) {
 
-//            echo "\n\nPREPARING TO SEND (".$attempts.")\n\n";
-//            var_dump([
-//                'Records' => $messages,
-//                'StreamName' => $this->stream
-//            ]);
+            $messages = array_map(function (array $message) {
+                return array_intersect_key($message, array_flip(['Data', 'PartitionKey']));
+            }, $messages);
 
-            $result = $this->client->putRecords([
-                'Records' => $messages,
-                'StreamName' => $this->stream
-            ]);
+            try {
+                $results = $this->client->putRecords([
+                    'Records' => $messages,
+                    'StreamName' => $this->stream
+                ]);
+            } catch (KinesisException $e) {
+                throw new Exception(self::ERR_UNKNOWN, 0, $e);
+            }
 
-            if (!isset($result['Records']) || !is_array($result['Records'])) {
+            if (!isset($results['Records']) || !is_array($results['Records'])) {
                 throw new Exception(self::ERR_RESPONSE);
             }
 
-            $messages = array_values(array_intersect_key($messages, array_filter($result['Records'], function (array $message) {
-                // filter out messages that were successfully sent
-                return (isset($message['SequenceNumber']) && isset($message['ShardId'])) ? false : true;
-            })));
+            $messages = array_values(array_filter(array_map(function (array $message, array $result) {
+                // merge original message with result
+                return array_merge($message, $result);
+            }, $messages, $results['Records']), function (array $message) {
+                // remove messages that were successfully sent
+                return isset($message['SequenceNumber']) && isset($message['ShardId']) ? false : true;
+            }));
 
-        } while (count($messages) > 0 && $attempts++ < $this->attempts);
+            $attempts++;
+        }
 
-        // handle errors
         if (count($messages) > 0) {
-            $this->error(sprintf(self::ERR_BATCH, count($messages), $total, $attempts));
+            $errors[] = sprintf(self::ERR_BATCH, count($messages), $total, $attempts, $this->formatErrors($messages));
+        }
+
+        if (count($errors) > 0) {
+            $this->handleErrors($errors);
         }
     }
 
     /**
      * Handle errors
      *
-     * @param string $message
+     * @param array $messages
      * @throws Exception
      */
-    private function error($message)
+    private function handleErrors(array $messages)
     {
         if ($this->silent) {
-            error_log($message);
-        } else {
-            throw new Exception($message);
+            foreach ($messages as $message) {
+                error_log($message);
+            }
         }
+
+        if (count($messages) > 1) {
+            throw new Exception(sprintf(self::ERR_MULTIPLE, count($messages), implode("\n", $messages)));
+        }
+
+        throw new Exception($messages[0]);
+    }
+
+    /**
+     * Format Kinesis error messages nicely
+     *
+     * @param array $messages
+     * @return string
+     */
+    private function formatErrors(array $messages)
+    {
+        return implode(', ', array_unique(array_map(function (array $message) {
+            return sprintf('%s (%s)', $message['ErrorCode'], $message['ErrorMessage']);
+        }, $messages)));
     }
 }
