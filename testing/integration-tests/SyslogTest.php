fc46991b191b1caf54728d889a471feaa3f74994
Steve Kluck <stevekluck@quickenloans.com>
2016-11-15 15:31:27 -0500

    Update integration tests

--- testing/integration-tests/SyslogTest.php
+++ testing/integration-tests/SyslogTest.php
@@ -1,26 +1,43 @@
 <?php
 
-namespace MCP\Logger;
+namespace QL\MCP\Logger;
 
-use MCP\Logger\Adapter\Psr\MessageFactory;
-use MCP\Logger\Renderer\JsonRenderer;
-use MCP\Logger\Service\SyslogService;
+use Composer\Autoload\ClassLoader;
+use QL\MCP\Logger\MessageInterface;
+use QL\MCP\Logger\Message\MessageFactory;
+use QL\MCP\Logger\Service\Serializer\LineSerializer;
+use QL\MCP\Logger\Service\SyslogService;
+
+if (!class_exists(ClassLoader::class)) {
+    $autoloader = require __DIR__ . '/../../vendor/autoload.php';
+}
 
 $ident = 'myapplication';
 $facility = LOG_USER;
 
-$service = new SyslogService(new JsonRenderer, ['ident' => $ident, 'facility' => $facility]);
-$fact = new MessageFactory;
-$fact->setDefaultProperty('environment', 'test');
+$service = new SyslogService(new LineSerializer, [
+    'ident' => $ident,
+    'facility' => $facility,
+    'silent' => false
+]);
 
-$logger = new Logger($service, $fact);
+$factory = new MessageFactory;
+$factory->setDefaultProperty(MessageInterface::SERVER_ENVIRONMENT, 'test');
+$factory->setDefaultProperty(MessageInterface::APPLICATION_ID, '200001');
 
-$logger->info('mcp-logger : syslog test 1');
-$logger->info('mcp-logger : syslog test 2');
+$logger = new Logger($service, $factory);
+
+// PLEASE NOTE:
+$logger->warning('mcp-logger : syslog test 1');
+$logger->warning('mcp-logger : syslog test 2');
 
 echo <<<HTML
 
 <br>Sent 2 log messages.
-<br>Check <code>/var/log/messages</code> for your messages
+
+<br>For LINUX: Check <code>/var/log/messages</code> for your messages.
+
+<br>For MACOS: Open <code>console.app</code> to check <code>system.log</code>.
+               Please note only message NOTICE and above are logged.
 
 HTML;
